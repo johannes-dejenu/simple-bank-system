14a810f9c35e1ea3d826f61cd15bbb3ce2d970af
John <johannesdejenu3@gmail.noreply.github.com>
2026-08-25 02:23:31 +0300

    initializing the class and the necessary attributes for BankAccount class

---
+++ BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount{
+    private int accountNumber;
+    private String accountName;
+    private int balance;
+
+    public BankAccount() {}
+    public BankAccount(int accountNumber, String accountName, int balance){
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    
+
+}
