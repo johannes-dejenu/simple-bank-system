12c76a11545a90a1708e26cb56166c430ab9a365
John <johannesdejenu3@gmail.noreply.github.com>
2026-08-26 01:10:29 +0300

    focusing on the abstract methods for the CurrentAccount class

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,8 +1,21 @@
 public class CurrentAccount extends BankAccount{
+    public CurrentAccount(int accountNumber, String accountName, int balance){
+        super(accountNumber, accountName, balance);
+    }
 
     @Override
     public void deposit(int amount){
-        
+        super.setBalance(super.getBalance() + amount);
+    }
+
+    @Override
+    public void withdraw(int amount){}
+
+
+    
+    @Override
+    public void displayInfo(){
+
     }
     
 }
