ef126ffd0a47e497ce42ef4e573be639ad6884fb
John <johannesdejenu3@gmail.noreply.github.com>
2026-08-26 01:19:39 +0300

    starting the SavingsAccount class

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,3 +1,15 @@
 public class SavingsAccount extends BankAccount{
+
+
+    @Override
+    public void deposit(int amount){}
+    @Override
+    public void withdraw(int amount){}
+    @Override
+    public void displayInfo(){}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
     
 }
